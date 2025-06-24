ce63e5a611d33cf19d8ba6bd1c47030d7f7dc157
Mary <comfys21208@gmail.com>
2025-06-24 16:58:29 +0300

    feat(main, start): Добавлен выбор языка для бота (английский или русский)

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 import os
 from dotenv import load_dotenv
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, ConversationHandler
+from start import start, IS_CHOOSING_LANGUAGE, NEXT_STATE
 
 load_dotenv()
 
@@ -9,8 +10,21 @@ def main():
     if not (TELEGRAM_TOKEN := os.getenv("TELEGRAM_BOT_TOKEN")): 
         print("Telegram token not found! Install telegram token!")
     else: 
-        app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
-        app.run_polling()
+
+        app_builder = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
+        states = {
+            IS_CHOOSING_LANGUAGE: [
+                CallbackQueryHandler(choosing_language, pattern="^bot_language"),
+            ],
+        }
+
+        handler = ConversationHandler(
+            entry_points=[CommandHandler("start", start)],
+            states=states, 
+            fallbacks=[CommandHandler("start", start)], 
+        )
+        application.add_handler(handler)
+        app_builder.run_polling()
 
 if __name__ == "__main__":
     main()
---
+++ src/start.py
@@ -0,0 +1,49 @@
+import sys
+
+from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
+from telegram.ext import ContextTypes
+
+import messages 
+
+IS_CHOOSING_LANGUAGE=1
+NEXT_STATE=2
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """
+        Реализация команды /start
+        Выбор языка пользователем
+    """
+    keyboard = [
+        [
+            InlineKeyboardButton("English", callback_data="interface_language_en"),
+            InlineKeyboardButton("Русский", callback_data="interface_language_ru"),
+        ],
+    ]
+    await context.bot.send_message(
+        update.effective_chat.id, "Select language for the bot\nВыберите язык для бота", reply_markup=InlineKeyboardMarkup(keyboard)
+
+    )
+    return IS_CHOOSING_LANGUAGE
+
+async def while_choosing_language(update: Update, context: ContextTypes.DEFAULT_TYPE): 
+    """
+        Приглашение пользователя выбрать язык интерфейса (английский или русский)
+    """
+    query = update.callback_query
+    await query.answer()
+
+    if not query.data.startswith("bot_language"):
+        print(f'An error occurred. Incorrect data was sent: {query.data}', file=sys.stderr)
+        return 
+    choosen_language = query.data[-2:]
+    if choosen_language not in ["en", "ru"]:
+        print(f'An error occurred. Incorrect data was sent: {query.data}', file=sys.stderr)
+        return
+    
+    context.user_data["bot_language"] = choosen_language
+
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,  
+        text=messages.USER_LANGUAGE[choosen_language] % choosen_language  
+    )
+    return NEXT_STATE
