20b86b5a6d522379dc1b769a95071adc85d91fa1
Mary <comfys21208@gmail.com>
2025-06-24 22:19:37 +0300

    feat(main, actions):Добавлена базовая реализация фичи для обработки изображений(удаления шума)

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, ConversationHandler
 from start import start, while_choosing_language, IS_CHOOSING_LANGUAGE, IS_CHOOSING_ACTION
+import actions 
 
 load_dotenv()
 
@@ -10,16 +11,20 @@ def main():
     if not (TELEGRAM_TOKEN := os.getenv("TELEGRAM_BOT_TOKEN")): 
         print("Telegram token not found! Install telegram token!")
     else: 
-
         app_builder = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
         states = {
             IS_CHOOSING_LANGUAGE: [
                 CallbackQueryHandler(while_choosing_language, pattern="^bot_language"),
             ],
             IS_CHOOSING_ACTION: [
-                CallbackQueryHandler(while_choosing_action, pattern="^bot_action"),
+                CallbackQueryHandler(remove_noise, pattern="^remove_noise"),
+                CallbackQueryHandler(generate_image, pattern="^generate_image"),
+                CallbackQueryHandler(view_history, pattern="^view_history"),
+                CallbackQueryHandler(magic, pattern="^magic"),
             ],
         }
+
+        states |= image_processing_states
 
         handler = ConversationHandler(
             entry_points=[CommandHandler("start", start)],
