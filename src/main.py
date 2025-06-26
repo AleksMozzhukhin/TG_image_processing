b14275746ba180e4a58bc815fb97b77fa26b3710
Mary <comfys21208@gmail.com>
2025-06-26 10:20:13 +0300

    fix(): Удаление лишнего

--- src/main.py
+++ src/main.py
@@ -8,10 +8,8 @@ from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
 from aiogram.client.bot import DefaultBotProperties
 
-from supabase import create_client, Client
-
 from routers.all_routers import all_routers
-from .db.db_wares import SupabaseMiddleware
+
 
 load_dotenv()
 
@@ -19,16 +17,12 @@ async def main():
     """Основная функция для запуска бота."""
     
     telegram_token = os.getenv("TELEGRAM_BOT_TOKEN")
-    supabase_url = os.getenv("SUPABASE_URL")
-    supabase_key = os.getenv("SUPABASE_KEY")
 
-    if not all([telegram_token, supabase_url, supabase_key]):
+    if not telegram_token:
         logging.critical("Не удалось загрузить все переменные окружения! (TOKEN, SUPABASE_URL, SUPABASE_KEY)")
         sys.exit(1)
 
-    supabase_client: Client = create_client(supabase_url, supabase_key)
     dispatcher = Dispatcher()
-    dispatcher.update.outer_middleware.register(SupabaseMiddleware(supabase_client))
     dispatcher.include_router(all_routers)
     bot = Bot(
         token=telegram_token,
